ac97b572ba67d004820d4b295da2759497d49ab8
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-05-20 18:48:21 +0200

    greedy mig corregit

--- Greedy.py
+++ Greedy.py
@@ -1,19 +1,20 @@
 import math
 
-# n: n ́umero de v ́ertices  del  grafo
-# c: diccionario  de costes , indexado  por  aristas (i,j) con i<j
+
+# n: number of points of the ground
+# c: Dictionary that saves the coordinates tuples
+# v0: Coordinate x of the last column
 
 def greedy(n, c, v0=0):
     T = []
     B = [v0]
     v = v0
     while len(B) < n:
-        minimo = 1e+100  # coste  infinito
+        minimum = 1e+100  # infinite cost
         for j in range(n):
-            # las  aristas  aparecen  en c como  tuplas (i,j) con i<j
-            coste = c[(min(v, j), max(v, j))]
-            if j not in B and coste < minimo:
-                minimo = coste
+            cost = c[(min(v, j), max(v, j))]
+            if j not in B and cost < minimum:
+                minimum = cost
                 w = j
     B.append(w)
     T.append((v, w))
