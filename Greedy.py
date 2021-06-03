11633ddc83a20b21b995976f6859aeec7d3bf8b3
aaron-at97 <arenas.aaron97@gmail.com>
2021-06-03 19:56:43 +0200

    Greedy "funcional" correcto

--- Greedy.py
+++ Greedy.py
@@ -3,62 +3,58 @@ import calculs
 import read_file
 
 
-def greedy(n):
+def greedy(distance_x, alt, height_aqueduct, alpha, beta):
     arbol = []
-    alt = [45, 63, 70, 61, 57]
     alture = []
-    distance = []
     impossible = True
     alture.append(alt[0])
-    arbol.append(n[0])
+    arbol.append(distance_x[0])
     minimo = 1e+100
     pos = 1
     i = 0
     # guardar_vec(arbol)
     print(arbol)
 
-    while pos != len(n) - 1:
+    while pos != len(distance_x) - 1:
 
         i = i + 1
-        if i < len(n):
-            arbol.append(n[i])
+        if i < len(distance_x):
+            arbol.append(distance_x[i])
             alture.append(alt[i])
             print(arbol)
             #print(alture)
             distance = calculs.obtain_distance(arbol)
-            cost, impossible = calculs.costs_aqueduct(len(arbol), 78, 72, 169, alture, distance)
+            cost, impossible = calculs.costs_aqueduct(len(arbol), alpha, beta, height_aqueduct, alture, distance)
             print(cost)
             print(minimo)
 
             if cost < minimo:
-                arbol.remove(n[i])
+                arbol.remove(distance_x[i])
                 alture.remove(alt[i])
                 minimo = cost
                 pos = i
             else:
-                arbol.remove(n[i])
+                arbol.remove(distance_x[i])
                 alture.remove(alt[i])
 
         else:
             i = pos
-            arbol.append(n[i])
+            arbol.append(distance_x[i])
             alture.append(alt[i])
             minimo = 1e+100
         #print(arbol)
         #print(pos)
-    arbol.append(n[pos])
+    arbol.append(distance_x[pos])
     alture.append(alt[pos])
     print(arbol)
     return minimo, impossible
 
 
 if __name__ == "__main__":
-    n = [7, 61, 67, 77, 87]
-    cost, impossible = greedy(n)
 
-    values, terrain_points, height_aqueduct, alpha, beta = read_file.read_file("testing/test5-2.in", data_separation=" ")
+    values, terrain_points, height_aqueduct, alpha, beta = read_file.read_file("testing/test5-5.in", data_separation=" ")
     distance, alt, distance_x = calculs.obtain_values(values)
-
+    cost, impossible = greedy(distance_x, alt, height_aqueduct, alpha, beta)
     print(cost)
 
 
