f180bd04c5b74d5b78c4b69380110c17982c9bd5
marc7666 <mcr99909@gmail.com>
2021-06-20 18:56:10 +0200

    read_file.py comentado y acabado

--- read_file.py
+++ read_file.py
@@ -17,10 +17,11 @@ def read_file(filename, data_separation=" "):
 
     print("************* Fichero cargado *************")
     values = []  # List of tuples (x, y)
-    # Openning file
-    with open(filename, "r") as vec_file_name:
+    # Opening file
+    with open(filename, "r") as vec_file_name:  # Opening the file in read mode
         # Strip lines
-        strip_reader = (line.strip() for line in vec_file_name)
+        strip_reader = (line.strip() for line in vec_file_name)  # Putting all the file in a list
+        # Putting all the lines of the file in a list
         filtered_reader = [line for line in strip_reader if line]
         # First line is the problem data
         terrain_points, height_aqueduct, alpha, beta = map(
@@ -28,6 +29,9 @@ def read_file(filename, data_separation=" "):
         )
         # Split lines, parse token and append to values (2 to last)
         for line in filtered_reader[1:]:
+            # Obtaining the coordinates of a point
             coordinate_x, coordinate_y = map(int, line.split(data_separation))
+            # Aggregating the coordinates in the list
             values.append((coordinate_x, coordinate_y))
+            # Returning the data file
         return values, terrain_points, height_aqueduct, alpha, beta
